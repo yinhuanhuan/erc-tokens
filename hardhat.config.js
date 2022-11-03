95a57b0abd2c86ac996f46977a44ed3b8ec083d1
yinhuanhuan <836699121@qq.com>
2022-11-03 19:15:25 +0800

    config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,43 @@
+require('dotenv').config()
+require("@nomicfoundation/hardhat-toolbox")
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
-};
+  defaultNetwork: "hardhat",
+
+  networks: {
+    hardhat: {
+    },
+    goerli: {
+      url: process.env.INFURA_API_URL,
+      accounts: [
+        process.env.OWNER_PRIVATE_KEY,
+      ],
+    },
+  },
+
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
+  },
+
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
+
+  mocha: {
+    timeout: 40000,
+  },
+}
